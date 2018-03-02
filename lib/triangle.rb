dfe3fbec76e8aa9241bfd57bdd52cbf02f9e2d1e
dannyb21892 <dannyb21892@verizon.net>
2018-03-02 12:42:20 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require "pry"
 class Triangle
-  # write code here
+
+  attr_reader :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+    @sides= [@one, @two, @three]
+  end
+
+  def kind
+    if (@sides.sort.last >= @sides.sort[0] + @sides.sort[1]) || @sides.include?(0)
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.invalid
+        raise TriangleError
+      end
+    elsif @one == @two && @two == @three
+      return :equilateral
+    elsif @sides.uniq == @sides
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def invalid
+    "Your triangle is invalid"
+  end
 end
